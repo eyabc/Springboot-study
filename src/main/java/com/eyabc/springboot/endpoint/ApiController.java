a98901e271f8cab78614b0d289e290021caf4c1f
eyabc <bey4314@naver.com>
2020-05-17 18:47:14 +0900

    @RequiredArgsController: 생성자주입, @RestController: Rest API 엔드포인트 구성, query(파라미터)로 검색어

--- src/main/java/com/eyabc/springboot/endpoint/ApiController.java
+++ src/main/java/com/eyabc/springboot/endpoint/ApiController.java
@@ -1,4 +1,19 @@
 package com.eyabc.springboot.endpoint;
 
+import com.eyabc.springboot.adapter.NaverAdapter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController("/api")
+@RequiredArgsConstructor
 public class ApiController {
+
+    private final NaverAdapter naverAdapter;
+
+    @GetMapping("/naver-shop")
+    public String searchShop(@RequestParam("query") String query) {
+        return naverAdapter.callSearchShop(query);
+    }
 }
