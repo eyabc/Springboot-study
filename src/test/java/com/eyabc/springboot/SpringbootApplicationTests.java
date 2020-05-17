5a2727920a6c49676326ac28df20d77e9f5aeea3
eyabc <bey4314@naver.com>
2020-05-17 14:41:32 +0900

    springboot init

---
+++ src/test/java/com/eyabc/springboot/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eyabc.springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
