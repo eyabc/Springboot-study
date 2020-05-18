56623d2f276c36cc61d0923f9c920fdd506fb415
eyabc <bey4314@naver.com>
2020-05-18 18:31:20 +0900

    네이버 shop 검색 api 호출

--- src/main/java/com/eyabc/springboot/adapter/NaverAdapter.java
+++ src/main/java/com/eyabc/springboot/adapter/NaverAdapter.java
@@ -1,10 +1,34 @@
 package com.eyabc.springboot.adapter;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.*;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-
-@RestController
+@Service
+@RequiredArgsConstructor
 public class NaverAdapter {
     private final String SEARCH_SHOP = "https://openapi.naver.com/v1/search/shop.json";
+    private final String clientId = NaverAdapterSecret.getClientID();
+    private final String clientSecret = NaverAdapterSecret.getClientSecret();
+    private final RestTemplate restTemplate;
+
+    public NaverAdapterResponse callSearchShop(String query) {
+
+        String url = SEARCH_SHOP + "?query=" + query;
+        HttpHeaders header = new HttpHeaders();
+        header.add(HttpHeaders.ACCEPT, MediaType.TEXT_HTML_VALUE);
+        header.add("Host", "openapi.naver.com");
+        header.add("X-Naver-Client-Id", clientId);
+        header.add("X-Naver-Client-Secret", clientSecret);
+
+        ResponseEntity<NaverAdapterResponse> response = restTemplate.exchange(
+          url, HttpMethod.GET, new HttpEntity(header), NaverAdapterResponse.class
+        );
+
+        return response.getBody();
+    }
+
+    @Component
+    public static class RestTemplate extends org.springframework.web.client.RestTemplate { }
 }
