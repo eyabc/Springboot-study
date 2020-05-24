bc1b98c0f1596669173c00f2dd72df2b9968017f
eyabc <bey4314@naver.com>
2020-05-24 17:35:00 +0900

    JsonParser, .json to javaObject util

---
+++ src/test/java/com/eyabc/springboot/UtilsTest.java
@@ -0,0 +1,21 @@
+package com.eyabc.springboot;
+
+import com.eyabc.springboot.adapter.response.Shop;
+import com.eyabc.springboot.utils.JsonFileParser;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UtilsTest {
+
+
+
+    @DisplayName("JSON Parser가 정상적으로 되고 있는지 확인하는 테스트")
+    @Test
+    void JSON_Parser_테스트 () {
+        Shop shop = JsonFileParser.parse("shop", Shop.class);
+        assertThat("Sun, 24 May 2020 15:38:15 +0900").isEqualTo(shop.getLastBuildDate());
+    }
+
+}
