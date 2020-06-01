f5d85768bba44cab11074c58a2d58d3cddf20f14
eyabc <bey4314@naver.com>
2020-06-01 14:01:35 +0900

    refactor: profile 에 따라 controller 분리

--- src/main/java/com/eyabc/springboot/controller/endpoint/NaverSearchController.java
+++ src/main/java/com/eyabc/springboot/controller/endpoint/NaverSearchController.java
@@ -1,30 +1,24 @@
 package com.eyabc.springboot.controller.endpoint;
 
 import com.eyabc.springboot.adapter.Adapter;
-import com.eyabc.springboot.dto.kakao.KakaoBookDto;
-import com.eyabc.springboot.dto.naver.NaverBookDto;
-import com.eyabc.springboot.dto.naver.NaverMovieDto;
 import com.eyabc.springboot.dto.naver.NaverShopDto;
+import com.eyabc.springboot.dto.search.BookDTO;
 import com.eyabc.springboot.dto.search.MovieDTO;
 import com.eyabc.springboot.service.search.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
+@Profile("naver")
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/search")
-@RequiredArgsConstructor
-public class SearchController {
-
+public class NaverSearchController {
     private SearchService searchService;
-    private final KakaoBookService kakaoBookService;
-    private final NaverBookService naverBookService;
-
-    private final Adapter adapter;
+    final Adapter adapter;
 
     @GetMapping("/shops")
     public NaverShopDto searchShop(@RequestParam("query") String query) {
@@ -33,16 +27,14 @@ public class SearchController {
     }
 
     @GetMapping("/movies")
-    public List<MovieDTO> searchMovie(@RequestParam("query") String query) {
+    public MovieDTO searchMovie(@RequestParam("query") String query) {
         searchService = new NaverMovieService(adapter);
         return searchService.getByQuery(query);
     }
 
     @GetMapping("/books")
-    public KakaoBookDto getBooks(@RequestParam("query") String query) {
-        return kakaoBookService.getByQuery(query);
+    public BookDTO getBooks(@RequestParam("query") String query) {
+        searchService = new NaverBookService(adapter);
+        return searchService.getByQuery(query);
     }
-
-    @GetMapping("/naver-books")
-    public NaverBookDto getNaverBooks(@RequestParam("query") String query) { return naverBookService.getByQuery(query); }
 }
