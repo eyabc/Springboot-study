c4f8838640b5c4902563943d5860f43ea34db774
eyabc <bey4314@naver.com>
2020-06-01 14:01:06 +0900

    refactor: 반환타입 일치

--- src/main/java/com/eyabc/springboot/facade/SearchFacade.java
+++ src/main/java/com/eyabc/springboot/facade/SearchFacade.java
@@ -1,19 +1,17 @@
 package com.eyabc.springboot.facade;
 
-import com.eyabc.springboot.dto.naver.NaverMovieDto;
+import com.eyabc.springboot.dto.search.MovieDTO;
 import com.eyabc.springboot.service.search.NaverMovieService;
 import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-@Slf4j
 @Component
 @AllArgsConstructor
 public class SearchFacade {
     private NaverMovieService naverMovieService;
 
-    public NaverMovieDto getNaverMoviesSortByUserRating(String query) {
-        NaverMovieDto movies = naverMovieService.getByQuery(query);
+    public MovieDTO getNaverMoviesSortByUserRating(String query) {
+        MovieDTO movies = naverMovieService.getByQuery(query);
         movies.getItems().sort((a, b) -> b.getUserRating() > a.getUserRating() ? 1 : -1);
         return movies;
     }
