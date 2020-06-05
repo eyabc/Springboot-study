0f08ca8eac9985745225ac1ec0ef7b53e375ae73
eyabc <bey4314@naver.com>
2020-06-05 14:46:28 +0900

    refactor: list 가공 로직을 메서드로 분리

--- src/main/java/com/eyabc/springboot/facade/SearchFacade.java
+++ src/main/java/com/eyabc/springboot/facade/SearchFacade.java
@@ -1,58 +1,33 @@
 package com.eyabc.springboot.facade;
 
 import com.eyabc.springboot.dto.search.MovieDTO;
+import com.eyabc.springboot.utils.MovieUtils;
 import com.eyabc.springboot.service.search.MovieService;
-import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
-import java.util.stream.Collectors;
-
-import static java.util.stream.Collectors.toList;
 
 @Component
-@AllArgsConstructor
 public class SearchFacade {
     public MovieDTO getMoviesSortByUserRating(MovieService movieService, String query) {
         MovieDTO movies = movieService.getByQuery(query);
-        movies.getItems().sort((a, b) -> b.getUserRating() > a.getUserRating() ? 1 : -1);
-        return movies;
+        List<MovieDTO.MovieItemDTO> newLists = MovieUtils.getListOrderRating(movies.getItems());
+
+        return MovieDTO.updateDTO(movies, newLists);
     }
 
     public MovieDTO getMoviesHavingUserRating(MovieService movieService, String query) {
         MovieDTO movies = movieService.getByQuery(query);
-        List filtered = movies.getItems()
-                .stream()
-                .filter(v -> v.getUserRating() != 0.0)
-                .collect(toList());
+        List<MovieDTO.MovieItemDTO> newLists = MovieUtils.getListHavingUserRating(movies.getItems());
 
-        return MovieDTO.builder()
-                .lastBuildDate(movies.getLastBuildDate())
-                .total(filtered.size())
-                .items(filtered)
-                .build();
+        return MovieDTO.updateDTO(movies, newLists);
     }
 
     public MovieDTO getMoviesRemovedTags(MovieService movieService, String query) {
         MovieDTO movies = movieService.getByQuery(query);
-        String tagPattern = "<b>|</b>";
+        List<MovieDTO.MovieItemDTO> newLists = MovieUtils.getListRemovedTags(movies.getItems());
 
-        return MovieDTO.builder()
-                .lastBuildDate(movies.getLastBuildDate())
-                .total(movies.getTotal())
-                .items(movies.getItems().stream()
-                        .map(item -> MovieDTO.MovieItemDTO.builder()
-                                .title(item.getTitle().replaceAll(tagPattern, ""))
-                                .link(item.getLink())
-                                .image(item.getImage())
-                                .subtitle(item.getSubtitle().replaceAll(tagPattern, ""))
-                                .pubDate(item.getPubDate())
-                                .director(item.getDirector())
-                                .actor(item.getActor())
-                                .userRating(item.getUserRating())
-                                .build())
-                        .collect(Collectors.toList()))
-                .build();
+        return MovieDTO.updateDTO(movies, newLists);
     }
 
 
